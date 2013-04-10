9c01d27477be8451dbea77d0548d05e90051d5d0
Jon Cram <jon@webignition.net>
2013-04-10 12:27:49 +0100

    test for multiple atom urls

--- tests/GetAtomUrlTest.php
+++ tests/GetAtomUrlTest.php
@@ -9,9 +9,21 @@ class GetAtomUrlTest extends BaseTest {
     public function testGetSingleAtomUrl() {
         $finder = new webignition\WebsiteRssFeedFinder\WebsiteRssFeedFinder();
         $finder->setHttpClient($this->getHttpClient());
-        $finder->setRootUrl('http://www.geekyportal.com/');
+        $finder->setRootUrl('http://www.example.com/');
         
         $this->assertEquals(array('http://www.geekyportal.com/feeds/posts/default'), $finder->getAtomFeedUrls());        
     }    
+    
+    
+    public function testGetMultipleAtomUrls() {
+        $finder = new webignition\WebsiteRssFeedFinder\WebsiteRssFeedFinder();
+        $finder->setHttpClient($this->getHttpClient());
+        $finder->setRootUrl('http://www.geekyportal.com/');
+        
+        $this->assertEquals(array(
+            'http://www.geekyportal.com/feeds/posts/default',
+            'http://www.geekyportal.com/feeds/posts/alternative'            
+        ), $finder->getAtomFeedUrls());        
+    }        
     
 }

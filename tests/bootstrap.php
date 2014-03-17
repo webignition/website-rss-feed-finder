43a8126cfb389d4788ca034b6f45e92ecc7101d6
Jon Cram <jon@webignition.net>
2014-03-17 23:34:29 +0000

    Update test bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,20 +1,29 @@
 <?php
-ini_set('display_errors', 'On');
-require_once  __DIR__ . '/../vendor/autoload.php';
 
-function autoload( $rootDir ) {
-    spl_autoload_register(function( $className ) use ( $rootDir ) {        
-        $file = sprintf(
-            '%s/%s.php',
-            $rootDir,
-            str_replace( '\\', '/', $className )
-        );        
-        
-        if ( file_exists($file) ) {
-            require $file;
-        }
-    });
+error_reporting(E_ALL | E_STRICT);
+
+// Ensure that composer has installed all dependencies
+if (!file_exists(dirname(__DIR__) . '/composer.lock')) {
+    die("Dependencies must be installed using composer:\n\nphp composer.phar install --dev\n\n"
+        . "See http://getcomposer.org for help with installing composer\n");
 }
 
-autoload( '/usr/share/php' );
-autoload( __DIR__ . '/');
+require_once 'PHPUnit/TextUI/TestRunner.php';
+
+// Include the phar files if testing against the phars
+//if (get_cfg_var('guzzle_phar')) {
+//    require get_cfg_var('guzzle_phar');
+//}
+
+// Include the composer autoloader
+$autoloader = require dirname(__DIR__) . '/vendor/autoload.php';
+
+//// Add the services file to the default service builder
+//$servicesFile = __DIR__ . '/Guzzle/Tests/TestData/services/services.json';
+//Guzzle\Tests\GuzzleTestCase::setServiceBuilder(Guzzle\Service\Builder\ServiceBuilder::factory($servicesFile));
+//
+//// Modify the include path so that it can find the Zend Framework
+//$paths = array('vendor/zend/zend-cache1', 'vendor/zend/zend-log1');
+//set_include_path(implode(PATH_SEPARATOR, array_map(function($path) {
+//    return __DIR__ . '/../' . $path;
+//}, $paths)) . PATH_SEPARATOR . get_include_path());

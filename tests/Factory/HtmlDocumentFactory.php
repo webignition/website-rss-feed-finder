2d619ba0f3b97889334e3e9dffa6c31e90010a0f
Jon Cram <jon@webignition.net>
2019-04-03 15:51:37 +0100

    Pass level 6

--- tests/Factory/HtmlDocumentFactory.php
+++ tests/Factory/HtmlDocumentFactory.php
@@ -6,6 +6,6 @@ class HtmlDocumentFactory
 {
     public static function load(string $name): string
     {
-        return file_get_contents(__DIR__ . '/../Fixtures/HtmlDocuments/' . $name . '.html');
+        return (string) file_get_contents(__DIR__ . '/../Fixtures/HtmlDocuments/' . $name . '.html');
     }
 }
